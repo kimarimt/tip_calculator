90380659b9f69cdaa068d7fc79b6c005ecae8b2e
Alec German <kimarimt@protonmail.com>
2025-03-06 08:12:09 -0500

    finished day 2

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def main():
-    print('Hello, World!')
+    print('Welcome to the Tip Calculator!')
+    bill = int(input('What was the total bill?: $'))
+    tip = int(input('How much tip would you like to give (10, 12, or 15)?: ')) / 100
+    people = int(input('How many people to split the bill?: '))
+    pay_per_person = ((bill * tip) + bill) / people
+    print(f'Each person should pay: ${pay_per_person:.2f}')
 
 
 if __name__ == '__main__':
